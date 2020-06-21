19b44b31b071e5d0a0403cf60ba423f8011ec908
Sven <sven@d-nite.de>
2020-06-21 22:37:22 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,46 @@
 import 'dotenv/config'
 import 'reflect-metadata'
-import { UserResolver } from "./resolver/UserResolver";
 import express from "express";
-import {ApolloServer} from "apollo-server-express";
-import {buildSchema} from "type-graphql";
 import cookieParser from 'cookie-parser';
 import {usermanager} from './usermanagement/usermanager'
 import cors from 'cors'
-import { BookingResolver } from './resolver/BookingResolver';
-import { PropertyResolver } from './resolver/PropertyResolver';
 import {imgUpload} from './imageHandling/imageUploader'
 import { getNodes } from './imageHandling/UploadNodes';
+/*
 import { ProductResolver } from './resolver/ProductResolver';
 import { PaymentResolver } from './payment/PaymentResolver';
 import { RatingResolver } from './resolver/RatingResolver';
 import { SightResolver } from './resolver/SightsResolver';
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import { BookingResolver } from './resolver/BookingResolver';
+import { PropertyResolver } from './resolver/PropertyResolver';
+import { UserResolver } from "./resolver/UserResolver";
+*/
 
 (async () => { 
     const app = express(); 
     app.use(cookieParser());
-
-    app.use(cors({
-        origin: "http://localhost:3000",
-        credentials: true
-    }))
+    
+    if(process.env.DEV){
+        app.use(cors({
+            origin: "http://localhost:3000",
+            credentials: true
+        }))
+    }
 
     app.use(express.static('public'));
     usermanager(app)
 
     app.post('/upload/property/image', );
 
-    imgUpload(app, getNodes())
+    app.get("/", (req, res) => {
+        console.log(req)
+        res.send("ready + env: DB " + process.env.COSMOS_DB_URL)
+    })
 
+    imgUpload(app, getNodes())
+    /*
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [
@@ -48,7 +57,7 @@ import { SightResolver } from './resolver/SightsResolver';
     });
     
     apolloServer.applyMiddleware({app, cors: false});
-
+    */
     app.listen(4000, () => {
         console.log('Server is listening on Port 4000')
     })
